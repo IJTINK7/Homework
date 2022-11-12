c841cca04d8bfb09aaef7f7f40b52a8566dd4441
IJTINK7 <ijtink7@mail.ru>
2022-11-12 20:06:09 +0300

    Create Table with "TopCars" component

--- micro-tasks/src/02-map/TopCars.tsx
+++ micro-tasks/src/02-map/TopCars.tsx
@@ -1,12 +1,29 @@
-type TopCarsType={
-	cars:Array<TopCarsArrayType>;
+type TopCarsType = {
+	cars: Array<TopCarsArrayType>;
 }
-type TopCarsArrayType ={
+type TopCarsArrayType = {
 	manufacturer: string;
 	model: string;
 }
-export const TopCars = (props:TopCarsType) => {
-	return(
-		<div>{props.cars}</div>
+export const TopCars = (props: TopCarsType) => {
+	return (
+		<table>
+			<tbody>
+			<tr>
+				<th>№</th>
+				<th>Manufacturer</th>
+				<th>Model</th>
+			</tr>
+			{props.cars.map((el, index) => {
+				return (
+					<tr>
+						<td key={index+1}>{index+1}</td>
+						<td key={index+1}>{el.manufacturer}</td>
+						<td key={index+1}>{el.model}</td>
+					</tr>
+				);
+			})}
+			</tbody>
+		</table>
 	);
 }

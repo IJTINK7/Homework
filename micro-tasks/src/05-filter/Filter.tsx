822bbcdae40bacc0bcf592db7135b6fa193f4e82
IJTINK7 <ijtink7@mail.ru>
2022-11-13 21:20:13 +0300

    Change "Filter" component

--- micro-tasks/src/05-filter/Filter.tsx
+++ micro-tasks/src/05-filter/Filter.tsx
@@ -1,5 +1,23 @@
-export const Filter =()=>{
+type FilterType={
+	money: Array<MoneyTypeArray>;
+}
+type MoneyTypeArray={
+	banknote: string;
+	nominal: number;
+	number: string;
+}
+export const Filter =(props:FilterType)=>{
 	return(
-		<div>hi</div>
+		<ul>
+			{props.money.map((objFromMoneyArr, index) => {
+				return (
+					<li key={index}>
+						<span> {objFromMoneyArr.banknote}</span>
+						<span> {objFromMoneyArr.nominal}</span>
+						<span> {objFromMoneyArr.number}</span>
+					</li>
+				)
+			})}
+		</ul>
 	);
 }

f4e2d56bc417e95a0e8fa6ca99a9c08d7a006d0a
IJTINK7 <ijtink7@mail.ru>
2022-12-14 18:12:51 +0300

    Completed changing styles

--- monday-counter-exam/src/Counter/Counter.tsx
+++ monday-counter-exam/src/Counter/Counter.tsx
@@ -19,27 +19,18 @@ export const Counter = () => {
 		setDisableResetButtonStatus(true);
 		setDisableIncreaseButtonStatus(false);
 	}
-	const displayColor = {
-		color: initialNumber === 5 ? "#e30043" : "#2b2b35",
-	}
-	const increaseButtonStyle = {
-		backgroundColor: disableIncreaseButtonStatus ? "#457685" : "#6ee4fe",
-		border: disableIncreaseButtonStatus ? "3px #457685 solid" : "3px #6ee4fe solid",
-	}
-	const resetButtonStyle = {
-		backgroundColor: disableResetButtonStatus ? "#457685" : "#6ee4fe",
-		border: disableResetButtonStatus ? "3px #457685 solid" : "3px #6ee4fe solid",
-	}
+	const display = initialNumber === 5 ? "RedDisplayValue": "";
 	return (
 		<div className={"Container"}>
-			<div style={displayColor} className={"Display"}>{initialNumber}</div>
+			<div className={`${display} Display`}>{initialNumber}</div>
 			<div className={"ButtonsContainer"}>
-				<button style={increaseButtonStyle}
-						className={"Button"}
+				<button
+						className={"IncreaseButton"}
 						onClick={() => {increaseNumberHandler(initialNumber)}}
 						disabled={disableIncreaseButtonStatus}>inc
 				</button>
-				<button style={resetButtonStyle}
+				<button
+						className={"ResetButton"}
 						onClick={resetDisplayHandler}
 						disabled={disableResetButtonStatus}>reset
 				</button>

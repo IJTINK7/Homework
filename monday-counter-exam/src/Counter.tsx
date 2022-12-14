4ad1e393551925c446fbb69f409faece827f9b57
IJTINK7 <ijtink7@mail.ru>
2022-12-14 15:04:53 +0300

    Adding full functional for buttons

--- monday-counter-exam/src/Counter.tsx
+++ monday-counter-exam/src/Counter.tsx
@@ -2,19 +2,28 @@ import React, {useState} from "react";
 
 export const Counter = () => {
 	const [initialNumber, setNewNumber] = useState(0);
-	// const [disableButtonStatus, setDisableButtonStatus] = useState(true)
+	const [disableIncreaseButtonStatus, setDisableIncreaseButtonStatus] = useState(false)
+	const [disableButtonStatus, setDisableButtonStatus] = useState(true)
 	const increaseNumberHandler =(displayNumber:number)=>{
 		setNewNumber(++displayNumber);
+		if (displayNumber === 1){
+			setDisableButtonStatus(false);
+		}
+		if(displayNumber === 5){
+			setDisableIncreaseButtonStatus(true);
+		}
 	}
 	const resetDisplayHandler =()=>{
 		setNewNumber(0);
+		setDisableButtonStatus(true);
+		setDisableIncreaseButtonStatus(false);
 	}
 	return (
 		<div className={"Container"}>
 			<div className={"Display"}>{initialNumber}</div>
 			<div className={"ButtonsContainer"}>
-				<button onClick={()=>{increaseNumberHandler(initialNumber)}} disabled={false}>inc</button>
-				<button onClick={resetDisplayHandler} disabled={true}>reset</button>
+				<button onClick={()=>{increaseNumberHandler(initialNumber)}} disabled={disableIncreaseButtonStatus}>inc</button>
+				<button onClick={resetDisplayHandler} disabled={disableButtonStatus}>reset</button>
 			</div>
 		</div>
 	);

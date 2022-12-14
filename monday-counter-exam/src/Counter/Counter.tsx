76e19724ad9a32eb30bdf702a7179fbe5f37204d
IJTINK7 <ijtink7@mail.ru>
2022-12-14 17:13:15 +0300

    Transferring necessary styles from component to css file. Finished the counter

--- monday-counter-exam/src/Counter/Counter.tsx
+++ monday-counter-exam/src/Counter/Counter.tsx
@@ -19,63 +19,28 @@ export const Counter = () => {
 		setDisableResetButtonStatus(true);
 		setDisableIncreaseButtonStatus(false);
 	}
-	const container = {
-		width: "400px",
-		height: "330px",
-		backgroundColor: "#2b2b35",
-		margin: "50px auto",
-		border: "3px #6ee4fe solid",
-		borderRadius: "10px,"
-	}
-	const display = {
-		width: "350px",
-		height: "170px",
-		backgroundColor: "#6ee4fe",
-		margin: "20px auto",
-		borderRadius: "10px",
-		fontSize: "120px",
-		fontWeight: "700",
+	const displayColor = {
 		color: initialNumber === 5 ? "#e30043" : "#2b2b35",
-	}
-	const buttonsContainer = {
-		width: "350px",
-		height: "90px",
-		margin: "20px auto",
-		border: "3px #6ee4fe solid",
-		borderRadius: "10px",
-		display: "flex",
-		justifyContent: "space-around",
-		alignItems: "center",
 	}
 	const increaseButtonStyle = {
 		backgroundColor: disableIncreaseButtonStatus ? "#457685" : "#6ee4fe",
 		border: disableIncreaseButtonStatus ? "3px #457685 solid" : "3px #6ee4fe solid",
-		borderRadius: "10px",
-		width: "120px",
-		height: "50px",
-		fontSize: "40px",
-		fontWeight: "700",
-		color: "#2b2b35",
 	}
 	const resetButtonStyle = {
 		backgroundColor: disableResetButtonStatus ? "#457685" : "#6ee4fe",
 		border: disableResetButtonStatus ? "3px #457685 solid" : "3px #6ee4fe solid",
-		borderRadius: "10px",
-		width: "120px",
-		height: "50px",
-		fontSize: "40px",
-		fontWeight: "700",
-		color: "#2b2b35",
 	}
 	return (
-		<div style={container}>
-			<div style={display}>{initialNumber}</div>
-			<div style={buttonsContainer}>
-				<button style={increaseButtonStyle} className={"Button"} onClick={() => {
-					increaseNumberHandler(initialNumber)
-				}} disabled={disableIncreaseButtonStatus}>inc
+		<div className={"Container"}>
+			<div style={displayColor} className={"Display"}>{initialNumber}</div>
+			<div className={"ButtonsContainer"}>
+				<button style={increaseButtonStyle}
+						className={"Button"}
+						onClick={() => {increaseNumberHandler(initialNumber)}}
+						disabled={disableIncreaseButtonStatus}>inc
 				</button>
-				<button style={resetButtonStyle} onClick={resetDisplayHandler}
+				<button style={resetButtonStyle}
+						onClick={resetDisplayHandler}
 						disabled={disableResetButtonStatus}>reset
 				</button>
 			</div>

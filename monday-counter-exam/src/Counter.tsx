ac9aedae561393718f8e610eaea98c1ed48b05c0
IJTINK7 <ijtink7@mail.ru>
2022-12-14 14:52:22 +0300

    Create functional for buttons with changing display after clicking on them

--- monday-counter-exam/src/Counter.tsx
+++ monday-counter-exam/src/Counter.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const Counter = () => {
+	const [initialNumber, setNewNumber] = useState(0);
+	// const [disableButtonStatus, setDisableButtonStatus] = useState(true)
+	const increaseNumberHandler =(displayNumber:number)=>{
+		setNewNumber(++displayNumber);
+	}
+	const resetDisplayHandler =()=>{
+		setNewNumber(0);
+	}
 	return (
 		<div className={"Container"}>
-			<div className={"Display"}>0</div>
-			<div>
-				<button>inc</button>
-				<button>reset</button>
+			<div className={"Display"}>{initialNumber}</div>
+			<div className={"ButtonsContainer"}>
+				<button onClick={()=>{increaseNumberHandler(initialNumber)}} disabled={false}>inc</button>
+				<button onClick={resetDisplayHandler} disabled={true}>reset</button>
 			</div>
 		</div>
 	);

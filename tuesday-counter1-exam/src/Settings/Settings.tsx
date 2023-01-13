8f73330712f7dd6f02b48b46b639a97db9ae2af4
IJTINK7 <ijtink7@mail.ru>
2023-01-13 08:19:08 +0300

    Adding styles for "Settings.tsx" component

--- tuesday-counter1-exam/src/Settings/Settings.tsx
+++ tuesday-counter1-exam/src/Settings/Settings.tsx
@@ -4,37 +4,23 @@ import './Settings.css';
 export const Settings = () => {
 	const [initialNumber, setNewNumber] = useState(0);
 	const [disableIncreaseButtonStatus, setDisableIncreaseButtonStatus] = useState(false)
-	const [disableResetButtonStatus, setDisableResetButtonStatus] = useState(true)
-	const increaseNumberHandler = (displayNumber: number) => {
-		setNewNumber(++displayNumber);
-		if (displayNumber === 1) {
-			setDisableResetButtonStatus(false);
-		}
-		if (displayNumber === 10) {
-			setDisableIncreaseButtonStatus(true);
-		}
-	}
-	const resetDisplayHandler = () => {
-		setNewNumber(0);
-		setDisableResetButtonStatus(true);
-		setDisableIncreaseButtonStatus(false);
-	}
-	const display = initialNumber === 10 ? "RedDisplayValue" : "";
+
 	return (
 		<div className={"SettingsContainer"}>
-			<div className={`${display} SettingsDisplay`}>{initialNumber}</div>
+			<div className="SettingsDisplay">
+				<div className="valuesContainer">
+					<h2>max value:</h2>
+					<input type="number" name="maxValue" id=""/>
+				</div>
+				<div className="valuesContainer">
+					<h2>start value:</h2>
+					<input type="number" name="startValue" id=""/>
+				</div>
+			</div>
 			<div className={"SettingsButtonsContainer"}>
 				<button
 					className={"IncreaseButton"}
-					onClick={() => {
-						increaseNumberHandler(initialNumber)
-					}}
-					disabled={disableIncreaseButtonStatus}>inc
-				</button>
-				<button
-					className={"ResetButton"}
-					onClick={resetDisplayHandler}
-					disabled={disableResetButtonStatus}>reset
+					disabled={disableIncreaseButtonStatus}>set
 				</button>
 			</div>
 		</div>
